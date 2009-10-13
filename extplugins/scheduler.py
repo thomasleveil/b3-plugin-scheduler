3dc4d2b65b45acc39bc28c8e5c7f8d15061b6554
Courgette <courgette@ubu-team.org>
2009-10-13 22:09:58 +0200

    v0.1.1

---
+++ extplugins/scheduler.py
@@ -0,0 +1,234 @@
+#
+# Plugin for BigBrotherBot(B3) (www.bigbrotherbot.com)
+# Copyright (C) 2008 Courgette
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#----------------------------------------------------------------------------
+#
+# Description:
+#
+# This plugin makes B3 send rcon command to your serveur at predefined time.
+#
+# Changelog:
+#
+# 07/06/2009 - 0.1.0 - Courgette
+# - initial release
+#
+# 07/06/2009 - 0.1.1 - Courgette
+# - fix spelling
+# - hours specified in config are now understood in the timezone setup in 
+#   the main B3 config
+#
+
+__version__ = '0.1.1'
+__author__  = 'Courgette'
+
+import thread, time, string, os
+import b3, b3.events, b3.plugin
+
+
+#--------------------------------------------------------------------------------------------------
+class SchedulerPlugin(b3.plugin.Plugin):
+  _tasks = None
+  _tzOffset = 0
+  
+  def onStartup(self):
+    pass
+    
+  def onLoadConfig(self):
+    
+    # remove eventual existing tasks
+    if self._tasks:
+      for t in self._tasks:
+        t.cancel()
+  
+    # Get time_zone from main B3 config
+    tzName = self.console.config.get('b3', 'time_zone').upper()
+    self._tzOffest = b3.timezones.timezones[tzName]
+  
+    # load cron tasks from config
+    self._tasks = []
+    for taskconfig in self.config.get('cron'):
+      try:
+        task = Task(self, taskconfig)
+        self._tasks.append(task)
+        self.info("cron task [%s] loaded" % task.name)
+      except Exception, e:
+        self.error(e)
+   
+    # load hourly tasks from config
+    for taskconfig in self.config.get('hourly'):
+      try:
+        task = HourlyTask(self, taskconfig)
+        self._tasks.append(task)
+        self.info("hourly task [%s] loaded" % task.name)
+      except Exception, e:
+        self.error(e)   
+        
+    # load daily tasks from config
+    for taskconfig in self.config.get('daily'):
+      try:
+        task = DaylyTask(self, taskconfig)
+        self._tasks.append(task)
+        self.info("daily task [%s] loaded" % task.name)
+      except Exception, e:
+        self.error(e)
+   
+
+    self.debug("%d tasks scheduled"% len(self._tasks))
+
+    
+
+ 
+  def onEvent(self, event):
+    pass
+    
+  def _convertCronHourToUTC(self, hourcron):
+    """
+    works with "*", "*/4", "5" or 9
+    """
+    if hourcron.strip() == '*': 
+      return hourcron
+    if '/' in hourcron:
+      h,divider = hourcron.split('/')
+      if h.strip() == '*':
+        return hourcron
+      UTChour = (int(h.strip()) - self._tzOffest)%24
+      return "%d/%s" % (UTChour, divider)
+    else:
+      return (int(hourcron.strip()) - self._tzOffest)%24
+ 
+ 
+ 
+class Task(object):
+  config = None
+  cronTab = None
+  plugin = None
+  name = None
+  minutes = None
+  hour = None
+  day = None
+  month = None
+  dow = None
+  
+  def __init__(self, plugin, config):
+    self.plugin = plugin
+    self.config = config
+    
+    self.name = config.attrib['name']
+    if not 'name' in config.attrib:
+      self.plugin.error("attribute 'name' not found in task")
+    else:
+      self.name = config.attrib['name']
+      
+    self.plugin.debug("setting up %s [%s]" % (self.__class__.__name__, self.name) )
+    for cmd in self.config.findall("rcon"):
+      self.plugin.debug("rcon : %s" % cmd.text)
+    self.schedule()
+    
+  def schedule(self):
+    """
+    schedule this task
+    """
+    self._getScheduledTime(self.config.attrib)
+    self.cronTab = b3.cron.PluginCronTab(self.plugin, self.runcommands, 
+      0, self.minutes, self.plugin._convertCronHourToUTC(self.hour), self.day, self.month, self.dow)
+    self.plugin.console.cron + self.cronTab
+    
+  def cancel(self):
+    """
+    remove this task from schedule
+    """
+    if self.cronTab:
+      self.plugin.info("canceling scheduled task [%s]" % self.name)
+      self.plugin.console.cron - self.cronTab
+    
+  def runcommands(self):
+    self.plugin.debug("running commands from %s" % self.name)
+
+    # send rcon commands
+    for cmd in self.config.findall("rcon"):
+      try:
+        self.plugin.console.write("%s"%cmd.text)
+      except Exception, e:
+        self.error(e)
+ 
+
+  def _getScheduledTime(self, attrib):
+
+    if not 'minutes' in attrib:
+      self.plugin.warning("attribute 'minutes' not found in task, using '*' as default")
+      self.minutes = '*'
+    else:
+      self.minutes = attrib['minutes']    
+      
+    if not 'hour' in attrib:
+      self.plugin.error("attribute 'hour' not found in task, using '*' as default")
+      self.hour = '*'
+    else:
+      self.hour = attrib['hour']
+      
+    if not 'day' in attrib:
+      self.plugin.error("attribute 'day' not found in task, using '*' as default")
+      self.day = '*'
+    else:
+      self.day = attrib['day']
+            
+    if not 'month' in attrib:
+      self.plugin.error("attribute 'month' not found in task, using '*' as default")
+      self.month = '*'
+    else:
+      self.month = attrib['month']
+      
+    if not 'dow' in attrib:
+      self.plugin.error("attribute 'dow' not found in task, using '*' as default")
+      self.dow = '*'
+    else:
+      self.dow = attrib['dow']
+      
+ 
+class HourlyTask(Task):
+  def _getScheduledTime(self, attrib):
+
+    if not 'minutes' in attrib:
+      self.plugin.debug("default minutes : 0. Provide a 'minutes' attribute to override")
+      self.minutes = 0
+    else:
+      self.minutes = attrib['minutes']    
+      
+    self.hour = '*'
+    self.day = '*'
+    self.month = '*'
+    self.dow = '*'
+    
+class DaylyTask(Task):
+  def _getScheduledTime(self, attrib):
+  
+    if not 'hour' in attrib:
+      self.plugin.debug("default hour : 0. Provide a 'hour' attribute to override")
+      self.hour = 0
+    else:
+      self.hour = attrib['hour']   
+      
+    if not 'minutes' in attrib:
+      self.plugin.debug("default minutes : 0. Provide a 'minutes' attribute to override")
+      self.minutes = 0
+    else:
+      self.minutes = attrib['minutes']    
+      
+    self.day = '*'
+    self.month = '*'
+    self.dow = '*'
+    
---
+++ readme.txt
@@ -0,0 +1,43 @@
+scheduler plugin for Big Brother Bot (www.bigbrotherbot.com)
+============================================================
+
+By #UbU#Courgette
+
+
+Description
+-----------
+
+This plugin as been made to allow you to easily setup scheduled rcon commands.
+
+Features :
+----------
+
+ * hourly tasks
+ * daily tasks
+ * cron like tasks (http://www.google.com/search?q=man+crontab+5)
+
+Installation
+------------
+
+ * copy scheduler.py into b3/extplugins
+ * copy scheduler.xml into b3/extplugins/conf
+ * update your main b3 config file with :
+
+<plugin name="scheduler" priority="18" config="@b3/extplugins/conf/scheduler.xml"/>
+
+
+Changelog
+---------
+
+07/06/2009 - v0.1.0 - Courgette
+- first public release. Waiting for feedbacks
+
+07/06/2009 - v0.1.1 - Courgette
+- correct spelling
+- hours specified in config are now understood in the timezone setup in the main B3 config
+
+  
+Support
+-------
+
+http://www.bigbrotherbot.com/forums/index.php?topic=947
